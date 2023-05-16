9b315c6f85c7a3ec21078d23812a037941e4445e
HM22-code <H.M-22@outlook.fr>
2023-05-16 13:14:42 +0200

    adding basic toolbar and footer core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-	declarations: [AppComponent, NavbarComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	declarations: [AppComponent, NavbarComponent, FooterComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatButtonModule,
+		MatIconModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
