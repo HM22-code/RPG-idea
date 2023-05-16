a23348c24b39ac2ff909c5792caa0429c132dbb0
HM22-code <H.M-22@outlook.fr>
2023-05-16 15:35:07 +0200

    adding navlist example in sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatListModule } from '@angular/material/list';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
@@ -33,6 +34,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 		MatButtonModule,
 		MatIconModule,
 		MatBadgeModule,
+		MatListModule,
 		DragDropModule
 	],
 	providers: [],
