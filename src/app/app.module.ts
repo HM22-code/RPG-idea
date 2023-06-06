5aff794bd0e69263ad1581ed6d9a4761d0158d9d
HM22-code <H.M-22@outlook.fr>
2023-06-06 16:02:19 +0200

    Adding all Material components imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,38 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCommonModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { ThemeSwitcherComponent } from './core/components/theme-switcher/theme-switcher.component';
 import { KanbanStatsComponent } from './kanban/pages/kanban-stats/kanban-stats.component';
 import { TaskDialogComponent } from './kanban/components/task-dialog/task-dialog.component';
@@ -42,8 +72,37 @@ import { TaskDialogComponent } from './kanban/components/task-dialog/task-dialog
 		MatIconModule,
 		MatBadgeModule,
 		MatListModule,
-		DragDropModule,
-		MatSlideToggleModule
+		MatSlideToggleModule,
+		MatGridListModule,
+		MatTabsModule,
+		MatCardModule,
+		MatDialogModule,
+		MatBottomSheetModule,
+		MatAutocompleteModule,
+		MatButtonToggleModule,
+		MatCheckboxModule,
+		MatChipsModule,
+		MatCommonModule,
+		MatDatepickerModule,
+		MatDividerModule,
+		MatExpansionModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatMenuModule,
+		MatPaginatorModule,
+		MatProgressBarModule,
+		MatProgressSpinnerModule,
+		MatRadioModule,
+		MatRippleModule,
+		MatSelectModule,
+		MatSliderModule,
+		MatSnackBarModule,
+		MatSortModule,
+		MatStepperModule,
+		MatTableModule,
+		MatTooltipModule,
+		MatTreeModule,
+		DragDropModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
