45c30ed61ebf9df8f21d762f3255cdc8ee841930
HM22-code <H.M-22@outlook.fr>
2023-11-14 17:45:44 +0100

    Adding dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ThemeService } from './core/services/theme.service';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'todolist-front';
+
+    @HostBinding('class') className = '';
+    title = 'todolist-front';
+    isDarkTheme!: Observable<boolean>;
+
+    constructor(
+        private themeService: ThemeService,
+        private overlay: OverlayContainer
+    ) {}
+
+    ngOnInit() {
+        this.isDarkTheme = this.themeService.isDarkTheme;
+        this.isDarkTheme.subscribe((darkMode) => {
+            const darkClassName = 'dark-theme';
+            this.className = darkMode ? darkClassName : '';
+            if (darkMode) {
+                this.overlay.getContainerElement().classList.add(darkClassName);
+            } else {
+                this.overlay.getContainerElement().classList.remove(darkClassName);
+            }
+        });
+    }
 }
